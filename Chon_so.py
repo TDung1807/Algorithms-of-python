d9ac5437d80fc2aee8e87ea58640e81c7e61e04c
TDung1807 <tiendunghuong4@gmail.com>
2025-08-20 14:04:15 +0700

    Lam them de Vinh Phuc 2021

---
+++ Chon_so.py
@@ -0,0 +1,17 @@
+
+#HSG Tin đề Vĩnh Phúc 2021
+
+
+a = int(input("Nhap a : "))
+b = int(input("Nhap b : "))
+score = ()
+if a > b :
+    score = a + (a -1)
+elif b > a : 
+    score = b + (b-1)
+elif a == b :
+    score = a + b 
+else : 
+    print ("cu phap loi !")
+
+print(score)
