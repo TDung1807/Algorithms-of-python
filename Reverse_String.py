6a85368ea2810e360c2329aa932a3340fb166738
TDung1807 <tiendunghuong4@gmail.com>
2025-08-28 00:27:06 +0700

    Lam them de

---
+++ Reverse_String.py
@@ -0,0 +1,13 @@
+def run():
+    Reversed_String = "".join(reversed(ori_string))
+    print(Reversed_String)
+
+
+    
+
+ori_string = input("String : ")
+
+if ori_string == "" :
+    print ("Invalid input")
+else : 
+    run()
---
+++ sum_of_input.py
@@ -0,0 +1,11 @@
+nums = input("nums : ")
+
+
+convert_input = list(map(int , nums))
+
+summ = sum(convert_input)
+
+
+
+print(summ)
+
